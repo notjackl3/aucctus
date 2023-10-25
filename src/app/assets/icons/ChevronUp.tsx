145c198f205e3a258031c287b971c11615a43c54
Tarek Sanger <tarek.sanger@me.com>
2023-10-25 15:25:02 -0400

    fix icons

--- src/app/assets/icons/ChevronUp.tsx
+++ src/app/assets/icons/ChevronUp.tsx
@@ -11,7 +11,7 @@ interface IconProps {
 const ChevronUp: FunctionComponent<IconProps> = ({ height, width, stroke }) => {
   return (
     <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15 12.5L10 7.5L5 12.5" stroke={stroke} stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M15 12.5L10 7.5L5 12.5" stroke={stroke} strokeWidth="2" stroke-linecap="round" strokeLinejoin="round" />
     </svg>
   )
 }
--- src/app/assets/icons/Download.tsx
+++ src/app/assets/icons/Download.tsx
@@ -12,7 +12,7 @@ const Download: FunctionComponent<IconProps> = ({ height, width, stroke }) => {
   return (
     <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`} fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M17.5 13V14C17.5 15.4001 17.5 16.1002 17.2275 16.635C16.9878 17.1054 16.6054 17.4878 16.135 17.7275C15.6002 18 14.9001 18 13.5 18H6.5C5.09987 18 4.3998 18 3.86502 17.7275C3.39462 17.4878 3.01217 17.1054 2.77248 16.635C2.5 16.1002 2.5 15.4001 2.5 14V13M14.1667 8.83333L10 13M10 13L5.83333 8.83333M10 13V3"
-        stroke={stroke} stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+        stroke={stroke} stroke-width="1.66667" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   )
 }
