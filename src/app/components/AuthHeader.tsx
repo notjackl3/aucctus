5d72f53e3d31ac374142f436662414847b75f0e8
Tarek Sanger <tarek.sanger@me.com>
2023-11-01 22:07:45 -0400

    auth header logo links to sign up

--- src/app/components/AuthHeader.tsx
+++ src/app/components/AuthHeader.tsx
@@ -1,11 +1,18 @@
 import { FunctionComponent } from "react";
 import styles from "../assets/styles/components/auth-header.module.scss";
 import Logo from "../assets/Logo.png";
+import { useNavigate } from "react-router-dom";
+import { AppPath } from "../../routes/routes";
 
 const AuthHeader: FunctionComponent = () => {
+  const navigate = useNavigate()
   return (
     <div className={styles.authHeader}>
-      <div className={styles.logo}>
+      <div className={styles.logo}
+        onClick={() => {
+          navigate(AppPath.SignIn)
+        }}
+      >
         <img
           alt="Logo"
           style={{ height: 30, width: 146 }}
