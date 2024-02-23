ac5350dc5266bee50da8d4005eb47e703796cd43
Tarek Sanger <tarek.sanger@me.com>
2024-02-23 09:23:26 -0600

    remove missing imports from pages

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,41 +2,12 @@ import Dashboard from './Dashboard';
 import Auth from './Auth';
 import NotFound from './NotFound';
 import Onboarding from './Onboarding';
-import IgniteConcept from './IgniteConcept';
-// import IgniteDomain from "./IgniteDomain";
-// import DomainList from "./DomainList";
-import GeneratedConcepts from './GeneratedConcepts';
-import ConceptOverview from './ConceptOverview';
-import ConceptList from './ConceptList';
-// import DomainMarket from "./DomainMarket";
-// import CustomerPersonas from "./CustomerPersonas/CustomerPersonas";
-// import ChallengeCenter from "./ChallengeCenter";
-// import ChallengeWizard from "./ChallengeWizard";
-// import ChallengeDetails from "./ChallengeDetails";
-// import IdeaSubmission from "./IdeaSubmission";
 
 const Page = {
   Auth,
   NotFound,
   Dashboard,
   Onboarding,
-
-  IgniteConcept,
-  GeneratedConcepts,
-  ConceptList,
-  ConceptOverview,
-  // CustomerPersonas,
-
-  // IgniteDomain,
-  // DomainList,
-  // DomainMarket,
-
-  // ChallengeCenter,
-  // ChallengeWizard,
-  // ChallengeDetails,
-
-  // IdeaOverview,
-  // IdeaSubmission,
 };
 
 export default Page;
