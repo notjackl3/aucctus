9c65aa1d53c3c5ec83ed2c5008dc164b7dd48c8f
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-03-05 14:00:12 -0700

    change ignite form to lightbulb icon

--- src/app/components/IgniteForm.tsx
+++ src/app/components/IgniteForm.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactNode } from 'react';
 
-import styles from '../assets/styles/pages/ignite.module.scss';
+import styles from '../pages/IgniteConcept/styles/igniteConcept.module.scss';
 import FeatureIcon from './FeatureIcon';
 
 interface IIgniteForm {
@@ -13,7 +13,7 @@ const IgniteForm: FunctionComponent<IIgniteForm> = ({ title, subtitle, children 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <FeatureIcon icon="file" color="purple" />
+        <FeatureIcon icon="lightbulb" color="purple" />
         <div className={styles.supportingText}>
           <h1 className={styles.title}>{title}</h1>
           <span className={styles.subtitle}>{subtitle}</span>
