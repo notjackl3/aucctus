b11b1f4ece6260bdafd0ed5a0eabb0bbb2c9f6a5
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-03-07 11:14:51 -0700

    give small styling option to Loading

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,9 +2,14 @@ import { FunctionComponent } from 'react';
 
 import styles from '../assets/styles/components/loading.module.scss';
 
-const Loading: FunctionComponent = () => {
+interface LoadingProps {
+  isSmall?: boolean;
+}
+const Loading: FunctionComponent<LoadingProps> = ({ isSmall }) => {
+  const smallDotsClassName = isSmall ? styles.small : '';
+
   return (
-    <span className={styles.bouncingDots}>
+    <span className={`${styles.bouncingDots} ${smallDotsClassName}`}>
       <span className={styles.dot}>.</span>
       <span className={styles.dot}>.</span>
       <span className={styles.dot}>.</span>
