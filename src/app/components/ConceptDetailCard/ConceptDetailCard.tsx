c1e745411e716168ec12ed8f3b056f2abd7a4dbb
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-03-14 10:25:25 -0600

    update ConceptDetailCard to handle action

--- src/app/components/ConceptDetailCard/ConceptDetailCard.tsx
+++ src/app/components/ConceptDetailCard/ConceptDetailCard.tsx
@@ -6,6 +6,9 @@ import Icon, { IconVariant } from '../Icon';
 interface ConceptDetailCardProps {
   title: string;
   children: ReactNode;
+  headerAction?: ReactNode;
+  cardClassName?: string;
+  headerClassName?: string;
   icon?: keyof typeof IconVariant;
 }
 
@@ -15,12 +18,21 @@ const iconDefaultProps = {
   stroke: '#2B3674',
 };
 
-const ConceptDetailCard: FunctionComponent<ConceptDetailCardProps> = ({ title, children, icon }) => {
+const ConceptDetailCard: FunctionComponent<ConceptDetailCardProps> = ({
+  title,
+  children,
+  icon,
+  cardClassName,
+  headerAction,
+}) => {
   return (
-    <div className={styles.conceptDetailCard}>
+    <div className={`${styles.conceptDetailCard} ${cardClassName ? cardClassName : ''}`}>
       <div className={styles.cardHeader}>
-        <span>{icon ? <Icon variant={icon} {...iconDefaultProps} /> : null}</span>
-        <h4>{title}</h4>
+        <span className={styles.headerTitle}>
+          <span>{icon ? <Icon variant={icon} {...iconDefaultProps} /> : null}</span>
+          <h4>{title}</h4>
+        </span>
+        {headerAction}
       </div>
       <div className={styles.content}>{children}</div>
     </div>
