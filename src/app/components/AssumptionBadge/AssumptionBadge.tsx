4a102524b89348bb66016ab63a03e70428d10cfc
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-03-20 12:30:35 -0600

    Add AssumptionBadge

---
+++ src/app/components/AssumptionBadge/AssumptionBadge.tsx
@@ -0,0 +1,49 @@
+import { FunctionComponent } from 'react';
+
+import styles from './styles/assumptionBadge.module.scss';
+import Icon from '../Icon';
+
+export enum AssumptionType {
+  adaptability = 'adaptability',
+  desirability = 'desirability',
+  feasibility = 'feasibility',
+  viability = 'viability',
+}
+
+export interface IConceptAssumptionBadgeProps {
+  assumptionType: AssumptionType;
+}
+
+const defaultIconProps = {
+  height: 24,
+  width: 24,
+  stroke: '#2B3674',
+};
+
+const AssumptionBadge: FunctionComponent<IConceptAssumptionBadgeProps> = ({ assumptionType }) => {
+  const assumptionColorObj = {
+    desirability: 'blue',
+    viability: 'green',
+    feasibility: 'yellow',
+    adaptability: 'blue',
+  };
+
+  const assumptionIconObj = {
+    desirability: <Icon variant="thermometer" {...defaultIconProps} />,
+    viability: <Icon variant="building" {...defaultIconProps} />,
+    feasibility: <Icon variant="filecode" {...defaultIconProps} />,
+    adaptability: <Icon variant="lineChartUp" {...defaultIconProps} />,
+  };
+
+  const color = assumptionColorObj[assumptionType];
+  const assumptionIcon = assumptionIconObj[assumptionType];
+
+  return (
+    <div className={`${styles.assumptionBadge} ${styles[`${color}Background`]}`}>
+      <span className={`${styles.icon} ${styles[`${color}Icon`]}`}>{assumptionIcon}</span>
+      <span className={styles.status}>{assumptionType}</span>
+    </div>
+  );
+};
+
+export default AssumptionBadge;
