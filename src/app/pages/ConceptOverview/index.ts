a5849619aee7d49447a6f9ff8e09d459724db8f1
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-03-25 13:45:39 -0600

    add Concept Pages as own Pages

--- src/app/pages/ConceptOverview/index.ts
+++ src/app/pages/ConceptOverview/index.ts
@@ -1,2 +1,16 @@
 import ConceptOverview from './ConceptOverview';
-export default ConceptOverview;
+import MarketDetails from './components/MarketDetails';
+import OverviewDetails from './components/OverviewDetails';
+import CustomerProfile from './components/CustomerProfile';
+import FinancialDetails from './components/FinancialDetails';
+import HypothesisDetails from './components/HypothesisDetails';
+
+const ConceptPages = {
+  ConceptOverview,
+  OverviewDetails,
+  MarketDetails,
+  CustomerProfile,
+  FinancialDetails,
+  HypothesisDetails,
+};
+export default ConceptPages;
