f42cf4a615375feb7eb1980b22f117cc6b9c379b
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-04-03 08:17:37 -0600

    refactor Quadrant Chart to use smaller components

--- src/app/components/QuadrantChart/QuadrantChart.tsx
+++ src/app/components/QuadrantChart/QuadrantChart.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent, useMemo } from 'react';
 
 import styles from './styles/quadrantChart.module.scss';
+import QuadrantChartGrid from './components/QuadrantChartGrid';
+import QuadrantChartActiveLines from './components/QuadrantChartActiveLine';
+import { getAdjustedCoord } from './utils';
 
 export interface ChartPoint {
   xCoord: number;
@@ -18,12 +21,16 @@ export interface QuadrantChartProps {
   xLeftLabel: string;
 }
 
-type LineDirection = 'horizontal' | 'vertical';
+export const CHART_WIDTH = 2000;
+export const CHART_CENTER_WIDTH = 1000;
+export const POINT_RADIUS = 48;
+export const valueConversionRate = 100;
 
-const CHART_WIDTH = 2000;
-const CHART_CENTER_WIDTH = 1000;
-const POINT_RADIUS = 48;
-const valueConversionRate = 100;
+const defaultTextProps = {
+  fill: '#667085',
+  fontFamily: 'Inter',
+  fontSize: '48',
+};
 
 const QuardantChart: FunctionComponent<QuadrantChartProps> = ({
   chartCoordinates,
@@ -38,10 +45,6 @@ const QuardantChart: FunctionComponent<QuadrantChartProps> = ({
     [chartCoordinates, selectedCoordinate]
   );
 
-  const getAdjustedCoord = (coordValue: number) => {
-    return CHART_CENTER_WIDTH + coordValue * valueConversionRate;
-  };
-
   const chartPoints = useMemo(() => {
     return chartCoordinates.map((coordinate, i) => {
       const isCoordSelected = coordinate.id === selectedCoordinate;
@@ -51,8 +54,8 @@ const QuardantChart: FunctionComponent<QuadrantChartProps> = ({
         <>
           <circle
             key={`coordinate-${i}`}
-            cx={yCoord}
-            cy={xCoord}
+            cx={xCoord}
+            cy={yCoord}
             r={POINT_RADIUS}
             fill={coordinate.color}
             strokeWidth={isCoordSelected ? '16' : undefined}
@@ -63,78 +66,10 @@ const QuardantChart: FunctionComponent<QuadrantChartProps> = ({
     });
   }, [chartCoordinates, selectedCoordinate]);
 
-  const renderActiveLines = (activeCoordinate?: ChartPoint) => {
-    if (!activeCoordinate) {
-      return;
-    }
-    const yCoord = getAdjustedCoord(activeCoordinate.yCoord);
-    const xCoord = getAdjustedCoord(activeCoordinate.xCoord);
-    return (
-      <>
-        {/* x-axis-line */}
-        <line
-          x1={CHART_CENTER_WIDTH}
-          y1={xCoord}
-          x2={yCoord - POINT_RADIUS}
-          y2={xCoord}
-          stroke={activeCoordinate.activeColor}
-          stroke-width="8"
-          stroke-dasharray="10,10"
-        />
-        {/* y-axis-line */}
-        <line
-          x1={yCoord}
-          y1={CHART_CENTER_WIDTH}
-          x2={yCoord}
-          y2={xCoord - POINT_RADIUS}
-          stroke={activeCoordinate.activeColor}
-          stroke-width="8"
-          stroke-dasharray="10,10"
-        />
-      </>
-    );
-  };
-
-  const renderGridLines = (numberGridLines: number, spacing: number, variant: LineDirection) => {
-    const lineList = [];
-    for (let i = 0; i <= numberGridLines; i++) {
-      const dist = i * spacing;
-      if (variant === 'horizontal') {
-        lineList.push(
-          <line
-            key={`horizontal-line-${i}`}
-            x1="0"
-            y1={dist}
-            x2={CHART_WIDTH}
-            y2={dist}
-            stroke="#7586a9"
-            strokeWidth="2"
-            strokeDasharray="5,5"
-          />
-        );
-      } else if (variant === 'vertical') {
-        lineList.push(
-          <line
-            key={`vertical-line-${i}`}
-            x1={dist}
-            y1={0}
-            x2={dist}
-            y2={CHART_WIDTH}
-            stroke="#7586a9"
-            strokeWidth="2"
-            strokeDasharray="5,5"
-          />
-        );
-      }
-    }
-    return lineList;
-  };
-
   return (
     <div className={styles.quadrantChart}>
       <svg viewBox={`0 0 ${CHART_WIDTH} ${CHART_WIDTH}`} xmlns="http://www.w3.org/2000/svg">
-        {renderGridLines(10, 200, 'horizontal')}
-        {renderGridLines(10, 200, 'vertical')}
+        <QuadrantChartGrid numberGridLines={10} spacing={200} />
         <line
           x1={CHART_CENTER_WIDTH}
           y1="0"
@@ -152,17 +87,17 @@ const QuardantChart: FunctionComponent<QuadrantChartProps> = ({
           stroke-width="10"
         />
         {chartPoints}
-        {renderActiveLines(activeCoordinate)}
-        <text x="1480" y="1120" font-family="Inter" font-size="48" fill="#667085">
+        <QuadrantChartActiveLines activeChartPoint={activeCoordinate} />
+        <text x="1480" y="1120" {...defaultTextProps}>
           {yTopLabel}
         </text>
-        <text x="80" y="1120" font-family="Inter" font-size="48" fill="#667085">
+        <text x="80" y="1120" {...defaultTextProps}>
           {yBottomLabel}
         </text>
-        <text x="1020" y="120" font-family="Inter" font-size="48" fill="#667085">
+        <text x="1020" y="120" {...defaultTextProps}>
           {xRightLabel}
         </text>
-        <text x="1020" y="1920" font-family="Inter" font-size="48" fill="#667085">
+        <text x="1020" y="1920" {...defaultTextProps}>
           {xLeftLabel}
         </text>
       </svg>
---
+++ src/app/components/QuadrantChart/components/QuadrantChartActiveLine.tsx
@@ -0,0 +1,48 @@
+import { FunctionComponent } from 'react';
+import { CHART_CENTER_WIDTH, ChartPoint } from '../QuadrantChart';
+import { getActiveAdjustmentDistance, getAdjustedCoord } from '../utils';
+
+export interface QuadrantChartActiveLinesProps {
+  activeChartPoint?: ChartPoint;
+}
+const defaultLineProps = {
+  strokeWidth: '8',
+  strokeDasharray: '10,10',
+};
+const QuadrantChartActiveLines: FunctionComponent<QuadrantChartActiveLinesProps> = ({ activeChartPoint }) => {
+  const renderActiveLines = (activeCoordinate?: ChartPoint) => {
+    if (!activeCoordinate) {
+      return;
+    }
+    const yCoord = getAdjustedCoord(activeCoordinate.yCoord);
+    const xCoord = getAdjustedCoord(activeCoordinate.xCoord);
+    const xAdjust = getActiveAdjustmentDistance(xCoord);
+    const yAdjust = getActiveAdjustmentDistance(yCoord);
+    return (
+      <>
+        {/* x-axis-line */}
+        <line
+          x1={CHART_CENTER_WIDTH}
+          y1={yCoord}
+          x2={xCoord + xAdjust}
+          y2={yCoord}
+          stroke={activeCoordinate.activeColor}
+          {...defaultLineProps}
+        />
+        {/* y-axis-line */}
+        <line
+          x1={xCoord}
+          y1={CHART_CENTER_WIDTH}
+          x2={xCoord}
+          y2={yCoord + yAdjust}
+          stroke={activeCoordinate.activeColor}
+          {...defaultLineProps}
+        />
+      </>
+    );
+  };
+
+  return <>{renderActiveLines(activeChartPoint)}</>;
+};
+
+export default QuadrantChartActiveLines;
