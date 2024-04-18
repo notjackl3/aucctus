2c1db3f024016eb06bcef461b3d9b76375ea1ebf
David Chan <davidchan@Davids-MacBook-Pro.local>
2024-04-18 12:22:57 -0600

    add variant to input-field

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -11,18 +11,31 @@ interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   isPassword?: boolean;
   required?: boolean;
   hint?: string;
+  width?: number | string;
+  variant?: 'settings';
 }
 
 const Input: ForwardRefRenderFunction<HTMLInputElement, InputFieldProps> = (
-  { label, name, hint, errorMessage, error = false, isPassword = false, ...props },
+  { label, name, hint, errorMessage, error = false, isPassword = false, variant, width, ...props },
   ref
 ) => {
   const showHint = !!hint || !!errorMessage;
   const hintText = errorMessage || hint;
 
+  function getInputFieldStyle(variant: InputFieldProps['variant']) {
+    switch (variant) {
+      case 'settings':
+        return styles.inputFieldSettings;
+      default:
+        return styles.inputField;
+    }
+  }
+
+  const inputFieldStyle = getInputFieldStyle(variant);
+
   return (
-    <div className={`${styles.inputField} ${error ? styles.inputFieldError : ''}`}>
-      <div className={styles.label}>{label}</div>
+    <div style={width ? { width } : {}} className={`${inputFieldStyle} ${error ? styles.inputFieldError : ''}`}>
+      {label && <div className={styles.label}>{label}</div>}
       <input {...props} type={isPassword ? 'password' : props.type ? props.type : 'text'} ref={ref} name={name} />
       {showHint ? <div className={styles.hintText}>{hintText}</div> : null}
     </div>
