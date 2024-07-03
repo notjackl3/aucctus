9cac58abb4d01587de52cb912633c6066eef0dec
Tarek Sanger <tarek.sanger@me.com>
2024-07-03 14:00:06 -0400

    fix loading screen

--- src/app/pages/LoadingScreen.tsx
+++ src/app/pages/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import Loading from '../components/Loading';
 
 const LoadingScreen: FunctionComponent = () => {
   return (
-    <div className='loading-screen'>
+    <div className='absolute left-0 top-0 z-[9999] m-auto flex h-full w-full items-center justify-center bg-white'>
       <Loading />
     </div>
   );
