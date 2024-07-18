0a9247053622a0fda6cb458b54fe62b39beb42c5
Tarek Sanger <tarek.sanger@me.com>
2024-07-18 17:25:34 -0400

    source button for market size

--- src/app/components/Cards/MarketSizeCard.tsx
+++ src/app/components/Cards/MarketSizeCard.tsx
@@ -23,15 +23,23 @@ const MarketSizeCard: React.FC<IMarketSizeCard> = ({
 }) => {
   return (
     <div className='inline-flex w-96 flex-col items-start justify-start rounded-xl border border-slate-200 bg-white shadow'>
-      <div className='flex h-24 flex-col items-start justify-start gap-5 self-stretch rounded-t-xl border-b border-gray-300 bg-white pt-5'>
+      <div className='flex h-28 flex-col items-start justify-start gap-5 self-stretch rounded-t-xl border-b border-gray-300 bg-white pt-5'>
         <div className='inline-flex items-center justify-center gap-2 self-stretch px-6'>
           <div className='flex items-center justify-start gap-2.5 pr-2.5'>
             <div className={`h-5 w-5 rounded-full ${bulletColor}`} />
           </div>
           <div className='w-56 text-base font-medium leading-normal text-indigo-900'>{title}</div>
-          <div className='inline-flex flex-col items-end justify-center'>
-            <p className='text-base font-medium leading-normal text-indigo-900'>{value}</p>
-            <p className='text-xs font-normal leading-none text-gray-500'>{descriptor}</p>
+          <div className='flex flex-col items-end justify-center gap-2.5'>
+            <div className='inline-flex flex-col items-end justify-center'>
+              <p className='text-base font-medium leading-normal text-indigo-900'>{value}</p>
+              <p className='text-xs font-normal leading-none text-gray-500'>{descriptor}</p>
+            </div>
+            <button
+              className='btn btn-primary-light btn-no-border h-8 w-8 border-violet-50 p-0 [&>svg>use]:stroke-primary-600'
+              onClick={onClick}
+            >
+              <Icon.Variant variant='book-open' strokeWidth={6} />
+            </button>
           </div>
         </div>
       </div>
@@ -41,15 +49,9 @@ const MarketSizeCard: React.FC<IMarketSizeCard> = ({
             key={generateRandomString(5)}
             className='inline-flex items-center justify-start gap-5 self-stretch border-b border-slate-200 bg-neutral-50 px-6 py-3 last:rounded-b-xl'
           >
-            <p className='w-64 self-stretch text-wrap text-sm font-medium leading-tight text-indigo-900'>
+            <p className='w-full self-stretch text-wrap text-sm font-medium leading-tight text-indigo-900'>
               {assumption}
             </p>
-            <button
-              className='btn btn-primary-light btn-no-border h-8 w-8 border-violet-50 p-0 [&>svg>use]:stroke-primary-600'
-              onClick={onClick}
-            >
-              <Icon.Variant variant='book-open' strokeWidth={6} />
-            </button>
           </div>
         ))}
       </div>
