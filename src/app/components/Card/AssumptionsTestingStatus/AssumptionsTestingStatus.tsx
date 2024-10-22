237e3616d4eba39feec7a53deae3229af661502e
Tarek Sanger <tarek.sanger@me.com>
2024-10-22 19:54:01 -0400

    fix layout of the assumptions overview screen

--- src/app/components/Card/AssumptionsTestingStatus/AssumptionsTestingStatus.tsx
+++ src/app/components/Card/AssumptionsTestingStatus/AssumptionsTestingStatus.tsx
@@ -1,4 +1,3 @@
-import { Select } from '@components';
 import { AssumptionCategory, IAssumptionTestStatus } from '@libs/api/types';
 import { ASSUMPTIONS_CATEGORIES } from '@libs/utils/concepts';
 import React from 'react';
@@ -15,19 +14,23 @@ const AssumptionTestingStatus: React.FC<AssumptionTestingStatusProps> = ({
   overview,
 }) => {
   return (
-    <div className='inline-flex h-auto flex-col items-start justify-start gap-7 rounded-lg border border-gray-200 bg-white px-6 py-8'>
+    <div className='inline-flex h-auto flex-col min-w-fit items-start justify-start gap-7 rounded-lg border border-gray-200 bg-white px-6 py-8'>
       {/* Header */}
       <div className='inline-flex items-center justify-between self-stretch bg-white'>
-        <div className='text-2xl font-semibold text-[#2b3674]'>
+        <div className='text-xl font-semibold text-[#2b3674]'>
           Assumption Testing Status
         </div>
-        <Select.TestingStatus value='notStarted' onChange={() => null} />
+        {/* <Select.TestingStatus
+          value='notStarted'
+          onChange={() => null}
+   
+        /> */}
       </div>
 
       {/* Content */}
       <div className='inline-flex h-auto flex-col items-start justify-start gap-3 bg-white'>
         {/* 'Table' Header */}
-        <div className=' inline-flex items-center justify-start gap-6 bg-white'>
+        <div className=' inline-flex min-w-fit items-center justify-start gap-6 bg-white'>
           <AssumptionTestingStatusHeader
             text='Category'
             className={'w-[134px]'}
