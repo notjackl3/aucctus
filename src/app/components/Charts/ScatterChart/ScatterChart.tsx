1a027817ff2991e33ae66649e99ead5f5b7a2c3d
Tarek Sanger <tarek.sanger@me.com>
2024-10-22 19:54:22 -0400

    update the scatter plot

--- src/app/components/Charts/ScatterChart/ScatterChart.tsx
+++ src/app/components/Charts/ScatterChart/ScatterChart.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './quadrantChart.module.scss';
 import ScatterChartActiveLines from './ScatterChartActiveLine';
 import ScatterChartGrid from './ScatterChartGrid';
 import { getNormalizedCoord } from './utils';
@@ -82,40 +81,59 @@ const ScatterChart: React.FC<ScatterChartProps> = ({
   }, [data, getPoints, selectedItem]);
 
   return (
-    <div className={styles.quadrantChart}>
+    <div className="flex self-start bg-white w-full">
       <svg
+        className="w-full h-auto max-w-full max-h-[500px]" // Max height is 500px but you can adjust
         viewBox={`0 0 ${CHART_SIZE} ${CHART_SIZE}`}
-        xmlns='http://www.w3.org/2000/svg'
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <ScatterChartGrid numberGridLines={10} spacing={200} />
+        <ScatterChartGrid numberGridLines={10} spacing={CHART_SIZE / 10} />
         <line
           x1={CHART_SIZE / 2}
-          y1='0'
+          y1="0"
           x2={CHART_SIZE / 2}
           y2={CHART_SIZE}
-          stroke='#E0E5F2'
-          strokeWidth='10'
+          stroke="#E0E5F2"
+          strokeWidth="10"
         />
         <line
-          x1='0'
+          x1="0"
           y1={CHART_SIZE / 2}
           x2={CHART_SIZE}
           y2={CHART_SIZE / 2}
-          stroke='#E0E5F2'
-          strokeWidth='10'
+          stroke="#E0E5F2"
+          strokeWidth="10"
         />
         {points}
         <ScatterChartActiveLines activeChartPoint={activePoint} />
-        <text x='1680' y='1120' {...defaultTextProps}>
+        <text
+          x={`${CHART_SIZE - 320}`}
+          y={`${CHART_SIZE / 2 + 80}`}
+          fill="#667085"
+          fontFamily="Inter"
+          fontSize="48"
+        >
           {xAxis.lowerLabel}
         </text>
-        <text x='80' y='1120' {...defaultTextProps}>
+        <text
+          x="80"
+          y={`${CHART_SIZE / 2 + 80}`}
+          {...defaultTextProps}
+        >
           {xAxis.upperLabel}
         </text>
-        <text x='1020' y='120' {...defaultTextProps}>
+        <text
+          x={`${CHART_SIZE / 2 - 40}`}
+          y="80"
+          {...defaultTextProps}
+        >
           {yAxis.upperLabel}
         </text>
-        <text x='1020' y='1920' {...defaultTextProps}>
+        <text
+          x={`${CHART_SIZE / 2 - 40}`}
+          y={`${CHART_SIZE - 80}`}
+          {...defaultTextProps}
+        >
           {yAxis.lowerLabel}
         </text>
       </svg>
