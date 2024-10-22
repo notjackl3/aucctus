b951f1d058d283711e7e80b3018da6b6d071e8a9
Tarek Sanger <tarek.sanger@me.com>
2024-10-22 19:54:59 -0400

    increase the size of the assumptions table text on the Test Model

--- src/app/components/Header/AssumptionsTable.tsx
+++ src/app/components/Header/AssumptionsTable.tsx
@@ -15,7 +15,7 @@ const AssumptionsTable: React.FC<AssumptionsTableProps> = ({
   return (
     <div className='inline-flex w-full items-center justify-between self-stretch border-b border-gray-200 px-3 py-2'>
       <div className='flex items-center justify-start gap-2.5 py-2.5'>
-        <Header.Two text={text} />
+        <Header.Two text={text} className='text-xl' />
         <Badge.Count value={count} />
       </div>
       <button onClick={handleAdd}>
