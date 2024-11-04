360ae57eacf738558e28fb5c50060bf77152c396
Tarek <tarek.sanger@me.com>
2024-11-04 10:22:14 -0500

    fix test steps use memo dependancy (#97)

--- src/app/hooks/tables/test-step-table.hook.tsx
+++ src/app/hooks/tables/test-step-table.hook.tsx
@@ -76,7 +76,7 @@ export const useTestStepTable = (testUuid: string, steps: ITestStep[] = []) => {
         ),
       }),
     ],
-    [],
+    [updateConceptTestStep],
   );
 
   const table = useReactTable({
