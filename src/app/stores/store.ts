3efd1c56c8d8a01c65268effa34b9041ed74d71f
Tarek <tarek.sanger@me.com>
2025-03-24 16:20:01 -0400

    multi-storage state with encyption (#201)

--- src/app/stores/store.ts
+++ src/app/stores/store.ts
@@ -1,19 +1,14 @@
 import { mergeDeep, withLenses } from '@dhmk/zustand-lens';
 import { create, StoreApi } from 'zustand';
-import {
-  createJSONStorage,
-  persist,
-  subscribeWithSelector,
-} from 'zustand/middleware';
+import { multiPersist } from 'zustand-multi-persist';
+import { createJSONStorage, subscribeWithSelector } from 'zustand/middleware';
 import authSlice, { IAuthState } from './auth/store';
 import conceptIncubationSlice, {
   IConceptIncubationState,
 } from './concept-incubation/store';
-import globalSlice, { IGlobalState } from './global/store';
 import { AucctusStorage } from './utils/storage';
-
 export interface IAppStore {
-  global: IGlobalState;
+  // global: IGlobalState;
   auth: IAuthState;
   incubation: IConceptIncubationState;
 }
@@ -25,33 +20,45 @@ export interface IStoreApi<S> {
 }
 
 const useStore = create<IAppStore>()(
-  persist(
+  multiPersist(
     subscribeWithSelector(
       withLenses({
-        global: globalSlice,
+        // global: globalSlice,
         auth: authSlice,
         incubation: conceptIncubationSlice,
       }),
     ),
     {
-      name: 'aucctus-store',
-      storage: createJSONStorage(
-        () =>
-          new AucctusStorage({
-            localStorage: ['auth'],
-            sessionStorage: ['global', 'incubation'],
-          }),
-      ),
-
-      // Only persist specific slices
-      partialize: (state) => ({
-        auth: state.auth,
-        global: state.global,
-        incubation: state.incubation,
-      }),
-      // Merge persisted state with current state to ensure all slices are hydrated
-      merge: (persistedState: unknown, currentState: IAppStore): IAppStore =>
-        mergeDeep(currentState, persistedState as IAppStore),
+      local: {
+        storage: createJSONStorage(
+          () =>
+            new AucctusStorage({
+              storage: 'localStorage',
+              encrypt: ['auth'],
+            }),
+        ),
+        partialize: (state) => ({
+          auth: {
+            access: state.auth.access,
+            refresh: state.auth.refresh,
+            user: state.auth.user,
+            account: state.auth.account,
+          },
+        }),
+        merge: (persistedState: unknown, currentState: IAppStore): IAppStore =>
+          mergeDeep(currentState, persistedState as IAppStore),
+      },
+      session: {
+        storage: createJSONStorage(() => sessionStorage),
+        partialize: (state) => ({
+          auth: {
+            initialized: state.auth.initialized,
+          },
+          incubation: state.incubation,
+        }),
+        merge: (persistedState: unknown, currentState: IAppStore): IAppStore =>
+          mergeDeep(currentState, persistedState as IAppStore),
+      },
     },
   ),
 );
