6a4cae4cb79c27ac7b6eee3bfdffcfa3a5922c53
jwduggan <81378003+jwduggan@users.noreply.github.com>
2025-03-25 18:24:15 -0400

    fix signup image (#215)

--- src/app/components/Auth/IntoSection.tsx
+++ src/app/components/Auth/IntoSection.tsx
@@ -4,9 +4,9 @@ import images from '../../assets/img';
 const IntoSection: FunctionComponent = () => {
   return (
     <div className='aucctus-bg-secondary flex h-screen flex-1 flex-col items-start justify-start gap-4 overflow-hidden text-left'>
-      <div className='aucctus-border-brand relative ml-16 mt-[15%] box-border flex h-[800px] w-[1200px] flex-col items-start justify-start rounded-lg border-4 border-solid'>
+      <div className='aucctus-border-brand relative ml-16 mt-[15%] box-border flex h-[800px] w-[1220px] flex-col items-start justify-start rounded-lg border-4 border-solid'>
         <img
-          className='h-[800px] w-[1200px] overflow-hidden rounded-lg object-contain'
+          className='h-[800px] w-[1220px] overflow-hidden rounded-lg'
           alt='Aucctus'
           src={images.screenMockup}
         />
