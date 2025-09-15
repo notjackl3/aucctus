e1885d148268245cf9793065a64e8828ec22ced3
jarrod-finnovate <jarrod@finnovate.io>
2025-09-15 10:43:32 -0400

    fix: AUC-906 user settings (login/logout bug) (#430)
    Co-authored-by: Jarrod Jack <jarrodjack@Jarrods-MacBook-Pro.local>

--- src/routes/guards/access.guard.tsx
+++ src/routes/guards/access.guard.tsx
@@ -1,19 +1,22 @@
 import useStore from '@stores/store';
 import { FunctionComponent, useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { AppPath } from '../routes';
+
+const ALLOWED_ROUTES = [AppPath.Onboarding];
 
 const AccessGuard: FunctionComponent = () => {
   const { user } = useStore((state) => state.auth);
+  const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (user) {
+    if (user && !ALLOWED_ROUTES.includes(location.pathname as AppPath)) {
       if (!user.firstName || !user.lastName) {
         navigate(AppPath.SettingsAbout);
       }
     }
-  }, [user, navigate]);
+  }, [user, navigate, location.pathname]);
 
   return <Outlet />;
 };
